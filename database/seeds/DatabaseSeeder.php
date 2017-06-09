df57e2528ab444043b04a8534a2262c480fec2e3
Acauan <acauanmp@gmail.com>
2017-06-09 00:05:01 -0300

    seeder para popular com dados fake

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model as Model; 
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->call(PostTableSeeder::class);
+
+        Model::reguard();
     }
 }
