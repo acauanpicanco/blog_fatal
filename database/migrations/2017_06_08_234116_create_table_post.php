d39c6bbb18c9647d916322fd1519ede642eb7c11
Acauan <acauanmp@gmail.com>
2017-06-09 00:06:46 -0300

    migrations da tabela post e adicao de coluna slug

---
+++ database/migrations/2017_06_08_234116_create_table_post.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo', 100);            
+            $table->longText('texto');
+            $table->string('imagem')->nullable()->default('img-post-default.jpg');
+            $table->string('tags', 100)->nullable()->index();
+            $table->tinyInteger('status')->default(1)->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
